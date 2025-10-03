a81ffca015b7baf7756506ff374a1166a905dfd6
CDasse <clemence.dasse@yahoo.fr>
2025-10-03 19:09:42 +0200

    feat: ajout du 03/10/2025
    création de la structure ellipse, ouverture d'un fichier svg et impression dans ce fichier de la forme d'une ellipse

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct ellipse_s
+{
+    int cx;
+    int cy;
+    int rx;
+    int ry;
+    char fill[10];
+} ellipse_t;
+
+
 int main(void)
 {
-    printf("Hello World !\n");
+
+    ellipse_t ellipse = {100, 50, 100, 50, "red"};
+    ellipse_t ellipse2 = {100, 50, 50, 25, "black"};
+    
+    char viewport[] = "<svg viewBox='0 0 200 100' xmlns='http://www.w3.org/2000/svg'>\n";
+    char end_viewport[] = "\n</svg>";
+
+    FILE *write_file = fopen("test.svg", "w");
+
+    if (fprintf(write_file, "%s", viewport) &&
+    fprintf(write_file, "<ellipse cx='%d' cy='%d' rx='%d' ry='%d' fill='%s' />", ellipse.cx, ellipse.cy, ellipse.rx, ellipse.ry, ellipse.fill) &&
+    fprintf(write_file, "<ellipse cx='%d' cy='%d' rx='%d' ry='%d' fill='%s' />", ellipse2.cx, ellipse2.cy, ellipse2.rx, ellipse2.ry, ellipse2.fill) &&
+    fprintf(write_file, "%s", end_viewport) <0) {
+        printf("Something went wrong");
+    }
+    
+    fclose(write_file);
+    
     return 0;
 }
